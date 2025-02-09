694ac0e5627a88f9eae64d56bbede4b4f60f9f0e
Yarmukhamed <157096252+Yarmukhamed@users.noreply.github.com>
2025-02-09 19:54:25 +0500

    Create 1.js

---
+++ lab3/JavaScript Fundamentals/variables/1.js
@@ -0,0 +1,31 @@
+let name = 'Jhon',age = 18, message='Hello';
+console.log(`Hello, my name is ${name}!`)
+/*variables
+1.var
+2.let
+3.const
+*/
+
+let messeage
+messeage = 'Hello';
+messeage = 'World'; /*changed */
+alert(messeage);
+
+
+/*const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00*/
+
+let name1='Akon'
+let admin = name1
+alert(admin);
+
+/*const birthday = '18.04.1982';
+
+const age1 = someCode(birthday);
+console.log(age1);*/
