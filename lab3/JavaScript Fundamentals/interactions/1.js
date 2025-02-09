46cd666ac3c7c0a048306d36995c5dac325830c8
Yarmukhamed <157096252+Yarmukhamed@users.noreply.github.com>
2025-02-09 19:57:26 +0500

    Create 1.js

---
+++ lab3/JavaScript Fundamentals/interactions/1.js
@@ -0,0 +1,8 @@
+let age = prompt ('How old are you?');
+console.log('Your age: '+age);
+
+/*let test = prompt ("Test",'');*/
+
+let isBoss = confirm("Are you the boss?");
+
+console.log( isBoss ); // true if OK is pressed
